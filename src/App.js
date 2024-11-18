8a5c6778fd4caf7635f4b01617e0984c5d9c7d4d
Apoorvs23 <Apoorv.Shamma@gmail.com>
2024-11-17 20:31:44 -0500

    feat: add restaurant card, details page, and protected routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "../src/components/AuthContext";
+import ProtectedRoute from "../src/components/ProtectedRoute";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Logout from "./components/Logout";
+import RestaurantDetailsPage from "./components/RestaurantDetailsPage";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<ProtectedRoute element={Home} />} />
+          <Route path="/logout" element={<ProtectedRoute element={Logout} />} />
+          <Route
+            path="/restaurant/:id"
+            element={<ProtectedRoute element={RestaurantDetailsPage} />}
+          />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
